1f01f943016e82c9d9114c17eba1b09199094c93
Bri Davis <bridavis@Bris-iMac.local>
2020-08-03 15:17:10 -0500

    pageObjects for checkpoint2.js

---
+++ pageObjects/checkpoint2Page.js
@@ -0,0 +1,10 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        splitButton: {
+            selector: '[name="evenOddButton"]',
+            locateStrategy: 'xpath'
+        }
+
+    }
+}
